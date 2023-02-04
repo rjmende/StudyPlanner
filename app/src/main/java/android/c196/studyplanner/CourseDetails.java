03aee755aae3b0a536955df3a2c7cb89104eadd6
robertmendez <robert.mendez2532@yahoo.com>
2023-02-03 18:41:10 -0600

    update 1

---
+++ app/src/main/java/android/c196/studyplanner/CourseDetails.java
@@ -0,0 +1,14 @@
+package android.c196.studyplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_details);
+    }
+}
